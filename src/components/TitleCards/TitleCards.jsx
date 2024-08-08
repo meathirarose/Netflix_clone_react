c24d82913d790049efce8f54714e04738f9b0907
meathirarose <athirarosejohn@gmail.com>
2024-08-08 13:51:00 +0530

    more cards added

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './TitleCards.css'
+import cards_data from '../../assets/cards/Cards_data'
 
 const TitleCards = () => {
+
+  const handleWheel = (e) => {
+    e.preventDefault();
+    cardsRef.current.scrollLeft+=e.deltaY;
+  }
+
+  const cardsRef = useRef();
+  useEffect(()=>{
+    cardsRef.current.addEventListener('wheel', handleWheel)
+  },[]) 
+
   return (
     <div className='titlecards'>
-      
+      <h2>Popular on Netflix</h2>
+      <div className="card-list" ref={cardsRef} >
+        {cards_data.map((card, index) => {
+           return <div key={index} className='card'>
+            <img src={card.image} alt="" />
+            <p>{card.name}</p>
+           </div>
+        })}
+      </div>
     </div>
   )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React from 'react';
+import Navbar from '../../components/Navbar/Navbar';
+import hero_banner from '../../assets/hero_banner1.webp';
+import hero_title from "../../assets/hero_title1.webp";
+import play_icon from "../../assets/play_icon.png";
+import info_icon from "../../assets/info_icon.png";
+import TitleCards from "../../components/TitleCards/TitleCards"
 import './Home.css'
 
 const Home = () => {
   return (
-    <div>
-      
+    <div className='home'>
+      <Navbar />
+
+      <div className="hero">
+        <img src={hero_banner} alt="" className="banner-img" />
+        <div className="hero-caption">
+          <img src={hero_title} alt="" className="caption-img" />
+          <p>When his prized possession gets stolen, a barber seeks help from <br /> the police, who jeer at his request - until they learn what 
+            they&apos;re <br /> really looking for.</p>
+          <div className="hero-btns">
+            <button className="btn" ><img src={play_icon} alt="" />Play</button>
+            <button className="btn dark-btn" ><img src={info_icon} alt="" />More Info</button>
+          </div>
+          <TitleCards />
+        </div>
+      </div>
+      <div className="more-cards">
+        <TitleCards />
+        <TitleCards />
+        <TitleCards />
+        <TitleCards />
+      </div>
     </div>
   )
 }
